74f9dd0521f8ecb917f1d24c4b61df1176b2df71
jhonathanjn <jn.tech.377@gmail.com>
2025-08-23 16:42:23 -0300

    first commit

---
+++ src/main/java/com/jhonathan/platformStreaming/PlatformStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.jhonathan.platformStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonathan/platformStreaming/PlatformStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonathan.platformStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
